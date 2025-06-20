a661dcec34e42d5311e39834a9de563cf5f6112e
Mildred <mildylicious@gmail.com>
2025-06-20 15:22:52 +0100

    Initial Django project setup

---
+++ mildred_and_bloom/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mildred_and_bloom project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mildred_and_bloom.settings')
+
+application = get_wsgi_application()
