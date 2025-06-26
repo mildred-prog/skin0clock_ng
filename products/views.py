cedc9d84f369e213773685e8ba54d9ce13f0e704
Mildred <mildylicious@gmail.com>
2025-06-26 19:21:58 +0100

    Refactor views to include product detail and home page functionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 # Create your views here.
 
@@ -13,3 +13,31 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
+
+def home(request):
+    """Home page with featured products by category"""
+    featured_categories = {
+        'cleansers': 1,
+        'toners': 2,
+        'serums': 3,
+        'moisturizers': 4,
+        'sunscreens': 5,
+    }
+    featured_products = {}
+    for key, cat_id in featured_categories.items():
+        featured_products[key] = Product.objects.filter(category_id=cat_id).order_by('-rating')[:1].first()
+    context = {
+        'featured_products': featured_products,
+    }
+    return render(request, 'home/index.html', context)
