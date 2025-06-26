b7ff5d91a08f96c73c6958ebad571d1a77fceea6
Mildred <mildylicious@gmail.com>
2025-06-26 19:21:39 +0100

    Implement featured products display on the home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 def index(request):
-    """ A view to return the index page """
-    
-    return render(request, 'home/index.html')
+    """Home page with featured products by category"""
+    featured_categories = {
+        'cleansers': 1,
+        'toners': 2,
+        'serums': 3,
+        'moisturizers': 4,
+        'sunscreens': 5,
+    }
+    featured_products = {}
+    for key, cat_id in featured_categories.items():
+        featured_products[key] = Product.objects.filter(category_id=cat_id).order_by('-rating')[:1].first()
+    context = {
+        'featured_products': featured_products,
+    }
+    return render(request, 'home/index.html', context)
