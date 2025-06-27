5fb3ee589b44b03979fea59fc455958cf8b608c4
Mildred <mildylicious@gmail.com>
2025-06-27 11:25:32 +0100

    Add admin configuration for FAQ model

---
+++ faq/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import FAQ
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'created_at')
+    search_fields = ('question', 'answer')
+    ordering = ('-created_at',) 
