0dbae00830dba6bdb36e1d89e4fd41f898664d37
Mildred <mildylicious@gmail.com>
2025-06-27 11:25:41 +0100

    Add FAQ model with fields for question, answer, and timestamps

---
+++ faq/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'FAQ'
+        verbose_name_plural = 'FAQs'
+
+    def __str__(self):
+        return self.question 
