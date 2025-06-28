f820319fcf985a2937e4f6c2deb6e518e65c8e86
Mildred <mildylicious@gmail.com>
2025-06-28 10:40:50 +0100

    Add URL pattern for wishlist app

--- skin0clock_ng/urls.py
+++ skin0clock_ng/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('faq/', include('faq.urls')),
+    path('wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
